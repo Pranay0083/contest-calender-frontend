90cf972bfe3fe741a73ef8e9a2a4f9d45157e6e4
Pranay Vishwakarma <149870314+Pranay0083@users.noreply.github.com>
2024-11-10 22:37:26 +0530

    Update contest.js

--- src/api/contest.js
+++ src/api/contest.js
@@ -1,5 +1,5 @@
 export async function getContest() {
-    const response = await fetch('https://contestcalenderserver.onrender.com/api/getAllContest');
+    const response = await fetch('https://scrappercontestcalendar.onrender.com/getAllContests');
     return await response.json();
 }
 
